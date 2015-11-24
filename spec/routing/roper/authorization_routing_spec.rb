0cfd18896f220e3ee681b1dc620ff5a70d55f05a
Brian Ploetz <bploetz@gmail.com>
2015-11-24 10:24:53 -0500

    Add missing routes to authorization routing spec

--- spec/routing/roper/authorization_routing_spec.rb
+++ spec/routing/roper/authorization_routing_spec.rb
@@ -8,6 +8,14 @@ module Roper
       it "routes to #authorize" do
         get("/authorize").should route_to("roper/authorization#authorize")
       end
+
+      it "routes to #approve_authorization" do
+        post("/approve_authorization").should route_to("roper/authorization#approve_authorization")
+      end
+
+      it "routes to #deny_authorization" do
+        post("/deny_authorization").should route_to("roper/authorization#deny_authorization")
+      end
     end
   end
 end
