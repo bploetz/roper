79785de430571443671c443fc1ad2d8f9de966e1
Brian Ploetz <bploetz@gmail.com>
2015-12-02 16:32:30 -0500

    Update migration to use a reference

--- db/migrate/20151124210620_create_oauth_client_redirect_uris.rb
+++ db/migrate/20151124210620_create_oauth_client_redirect_uris.rb
@@ -1,12 +1,11 @@
 class CreateOauthClientRedirectUris < ActiveRecord::Migration
   def change
     create_table :oauth_client_redirect_uris do |t|
-      t.integer :client_id
       t.string :uri
 
       t.timestamps
     end
 
-    add_index :oauth_client_redirect_uris, :client_id
+    add_reference :oauth_client_redirect_uris, :client, index: true
   end
 end
