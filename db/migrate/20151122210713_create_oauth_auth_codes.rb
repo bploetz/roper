42dba39fa9a4293c3240f3719b70d466b8fe159b
Brian Ploetz <bploetz@gmail.com>
2015-12-09 11:31:30 -0500

    Make auth codes belong to clients

--- db/migrate/20151122210713_create_oauth_auth_codes.rb
+++ db/migrate/20151122210713_create_oauth_auth_codes.rb
@@ -2,7 +2,6 @@ class CreateOauthAuthCodes < ActiveRecord::Migration
   def change
     create_table :oauth_auth_codes do |t|
       t.string :code, null: false
-      t.string :client_id, null: false
       t.string :redirect_uri, null: false
       t.boolean :redeemed, null: false, default: false
       t.datetime :expires_at, null: false
@@ -10,8 +9,10 @@ class CreateOauthAuthCodes < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_reference :oauth_auth_codes, :client, index: true
+    # work around for quirk in sqlite where you can add a not null column after the table is created.
+    change_column :oauth_auth_codes, :client_id, :integer, :null => false
     add_index :oauth_auth_codes, :code, :unique => true
-    add_index :oauth_auth_codes, :client_id
     add_index :oauth_auth_codes, :expires_at
   end
 end
--- lib/roper/repository/active_record/authorization_code.rb
+++ lib/roper/repository/active_record/authorization_code.rb
@@ -2,6 +2,8 @@ module Roper
   module ActiveRecord
     class AuthorizationCode < ::ActiveRecord::Base
       self.table_name = "oauth_auth_codes"
+
+      belongs_to :client
     end
   end
 end
