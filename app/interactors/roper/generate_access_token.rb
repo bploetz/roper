7223f01f5ba8a0fafda0658a28858129d9934afe
Brian Ploetz <bploetz@gmail.com>
2015-12-10 17:19:26 -0500

    move things to be in the right module namespace

---
+++ app/interactors/roper/generate_access_token.rb
@@ -0,0 +1,14 @@
+module Roper
+  class GeneratAccessToken
+    include Interactor
+
+    def call
+      access_token = Roper::Repository.for(:access_token).new(:client_id => context.client.id)
+      if access_token.save
+        context.access_token = access_token
+      else
+        context.fail!
+      end
+    end
+  end
+end
