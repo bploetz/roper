e63f39a63e603819b0c44e87416871a82c6eaca5
Brian Ploetz <bploetz@gmail.com>
2015-12-11 15:38:11 -0500

    Honor expiration configration when generating access tokens

--- app/interactors/roper/generate_access_token.rb
+++ app/interactors/roper/generate_access_token.rb
@@ -6,6 +6,9 @@ module Roper
 
     def call
       access_token = Roper::Repository.for(:access_token).new(:client_id => context.client.id)
+      if Roper.access_token_expiration_time
+        access_token.expires_at = Roper.access_token_expiration_time.send(:seconds).from_now
+      end
       if access_token.save
         context.access_token = access_token
       else
--- spec/interactors/roper/generate_access_token_spec.rb
+++ spec/interactors/roper/generate_access_token_spec.rb
@@ -16,6 +16,7 @@ module Roper
           access_token = Roper::Repository.for(:access_token).model_class.first
           expect(access_token).not_to eq(nil)
           expect(access_token.client_id).to eq(client.id)
+          expect(access_token.expires_at).not_to eq(nil)
         end
 
         it "sets the created access token on the context" do
@@ -23,10 +24,25 @@ module Roper
         end
       end
 
+      context "when Roper.access_token_expiration_time is nil" do
+        it "does not set expires_at on the access token" do
+          previous_value = Roper.access_token_expiration_time
+          Roper.access_token_expiration_time = nil
+          context
+          access_token = Roper::Repository.for(:access_token).model_class.first
+          expect(access_token).not_to eq(nil)
+          expect(access_token.client_id).to eq(client.id)
+          expect(access_token.expires_at).to eq(nil)
+          Roper.access_token_expiration_time = previous_value
+        end
+      end
+
+
       context "when creating the access token fails" do
         let(:stub_access_token) { double("access token") }
 
         before :each do
+          allow(stub_access_token).to receive(:expires_at=)
           expect(stub_access_token).to receive(:save).and_return(false)
           expect(Roper::Repository.for(:access_token)).to receive(:new).and_return(stub_access_token)
         end
